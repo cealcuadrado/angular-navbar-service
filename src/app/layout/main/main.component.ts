8fa0a1ce97c2f452a309904157a2c8221ff472a6
CeAlCuadrado <cealcuadrado@outlook.com>
2021-06-16 11:45:34 -0400

    Tamaño de títulos y párrafos cambia desde la vista main, en base al tamaño del servicio navbar

--- src/app/layout/main/main.component.ts
+++ src/app/layout/main/main.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { NavbarService } from '../navbar/navbar.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private navbar: NavbarService
+  ) { }
 
   ngOnInit(): void {
+
   }
 
+  definirNivelTamannio(): string {
+    return `nivel-${this.navbar.obtenerNivelTamannio()}`;
+  }
 }
